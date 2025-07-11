0d1c08ff79ede7da5dcf0444dc9e60bf810c6e13
Abhishek-pratap9120 <abhi911387@gmail.com>
2025-07-11 15:24:00 +0530

    added rg

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rgtest" {
     name="rg121"
     location="central india" 
 }
+resource "azurerm_resource_group" "rgtest1" {
+    name="rg1212"
+    location="central india" 
+}
