352a661dae12180bd1ff66ebe55790beb25d9d0c
Abhishek-pratap9120 <abhi911387@gmail.com>
2025-07-11 16:23:26 +0530

    sg added

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ resource "azurerm_resource_group" "rgtest" {
     name="rg121"
     location="central india" 
 }
+
+resource "azurerm_storage_account" "sg" {
+    name = "sg121"
+    resource_group_name = azurerm_resource_group.rgtest.name
+    location = azurerm_resource_group.rgtest.location
+    account_tier = "Standard"
+    account_replication_type = "GRS"
+  
+}
